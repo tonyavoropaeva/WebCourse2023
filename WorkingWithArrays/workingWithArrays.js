f4bfa6da5daf64fae64ffec224b5c96e7f813c35
tonyavoropaeva <voropaevatonya@gmail.com>
2024-03-26 12:04:51 +0300

    Исправила задачу "Работа с массивами"

--- WorkingWithArrays/workingWithArrays.js
+++ WorkingWithArrays/workingWithArrays.js
@@ -1,61 +1,59 @@
+~function () {
 // Создайте массив чисел
-let array = [3, 20, 11, 7, 15, 8, 41, 2, 9, 30, 17, 1];
+    let array = [3, 20, 11, 7, 15, 8, 41, 2, 9, 30, 17, 1];
 
 // Отсортируйте его по убыванию
-function sortArrayInDescendingOrder(array) {
-    let result;
+    const sortNumbersArrayInDescendingOrder = numbersArray =>
+        numbersArray.sort(function (e1, e2) {
+            return e2 - e1;
+        });
 
-    result = array.sort(function (e1, e2) {
-        return e1 - e2;
-    });
-
-    return result;
-}
-
-console.log("Отсортированный массив: " + sortArrayInDescendingOrder(array));
+    sortNumbersArrayInDescendingOrder(array);
+    console.log("Отсортированный массив: " + array);
 
 // Получите подмассив из первых 5 элементов
-function subarrayOfTheFirstFiveElements(array) {
-    return array.slice(0, 5);
-}
+    const getFirstFiveElements = array => {
+        return array.slice(0, 5);
+    }
 
-console.log("Первые пять елементов массива: " + subarrayOfTheFirstFiveElements(array));
+    console.log("Первые пять элементов массива: " + getFirstFiveElements(array));
 
 // подмассив из последних 5 элементов
-function subarrayOfTheLastFiveElements(array) {
-    return array.slice(array.length - 5);
-}
+    const getLastFiveElements = array => {
+        return array.slice(-5);
+    }
 
-console.log("Последние пять елементов массива: " + subarrayOfTheLastFiveElements(array));
-
+    console.log("Последние пять элементов массива: " + getLastFiveElements(array));
 
 // Найдите сумму элементов массива, которые являются
 // четными числами
-function evenArrayElementsSum(array) {
-    let evenNumbersArray = evenArrayElements(array);
+    const sumEvenNumbers = numbersArray => {
+        return getEvenNumbers(numbersArray).reduce(function (total, amount) {
+            return total + amount;
+        }, 0);
+    }
 
-    return evenNumbersArray.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-}
-
-console.log("Сумма четных элементов массива: " + evenArrayElementsSum(array));
+    console.log("Сумма четных элементов массива: " + sumEvenNumbers(array));
 
 // Создайте массив чисел от 1 до 100, в таком порядке
-let arrayOneHundred = [...Array(100)].map((_, i) => i + 1);
+    const hundredNumbersArray = [];
 
-console.log(arrayOneHundred);
+    for (let i = 0; i < 100; ++i) {
+        hundredNumbersArray.push(i + 1);
+    }
+
+    console.log(hundredNumbersArray);
 
 // Получите список квадратов четных чисел из этого массива
-function evenNumbersInArraySquaresList(array) {
-    let evenNumbersArray = evenArrayElements(array);
-    return evenNumbersArray.map((x) => x * 2);
-}
+    const getEvenNumbersSquares = numbersArray => {
+        return getEvenNumbers(numbersArray).map(x => x * x);
+    }
 
-console.log(evenNumbersInArraySquaresList(arrayOneHundred));
+    console.log(getEvenNumbersSquares(hundredNumbersArray));
 
-function evenArrayElements(array) {
-    return array.filter(function (number) {
-        return number % 2 === 0;
-    });
-}
+    function getEvenNumbers(numbersArray) {
+        return numbersArray.filter(function (number) {
+            return number % 2 === 0;
+        });
+    }
+}();
